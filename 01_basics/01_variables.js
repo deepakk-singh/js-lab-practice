e0fc3169764c8fc3ec7baf8af12bc2c238693012
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-26 04:14:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+/*const accountId=144356;
+let accountEmail="abcs@hd45";
+var accountPassword="45798";
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountId)
+*/
+accountId=14576
+accountEmail="absc@gmail.com"
+accountPassword="bd131"
+accountCity="Chennai"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
