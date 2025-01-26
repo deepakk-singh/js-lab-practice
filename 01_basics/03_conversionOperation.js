7d453a821e8a4d3026b797004cb0285b6f891ada
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-26 09:18:06 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+/*
+let score=33
+console.log(score)
+console.log(typeof score)         we can also write as in js
+console.log(typeof(score)) */
+
+
+//conversion 
+/*
+let score="33"
+console.log(score)
+console.log(typeof score)
+
+let valueInNumber= Number(score)   //we can also use as string
+console.log(typeof valueInNumber)
+
+
+let score=null
+console.log(score)
+console.log(typeof score)
+
+let valueInNumber= Number(score)   //we can also use as string
+console.log(typeof valueInNumber)
+
+*/
+
+let loggedIn=1
+console.log(typeof loggedIn)
+let booleanIsNumber=Boolean(loggedIn)
+console.log(booleanIsNumber)
+
+//when we write as
+//  1 => then we get true
+//  0 => then we get false 
+//  but "" => then we get false
+// and "hfdsfn" => also get true
