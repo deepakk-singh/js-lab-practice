7414498f532b891c5bb5dcc6ecd15d1a701d82b0
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-26 04:41:09 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,12 @@ console.log(accountEmail)
 console.log(accountPassword)
 console.log(accountId)
 */
+
+/*
+prefer not to  use var
+beacause of issue in block scope and functional scope 
+
+*/
 accountId=14576
 accountEmail="absc@gmail.com"
 accountPassword="bd131"
