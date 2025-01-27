f54c3180dadd1e28b95fd6c675a0529305886664
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-27 17:29:39 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,15 @@
+const name="Deepak"
+const repoCount=50
+/*
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`)
+
+console.log(name[0])      //Accessing Characters: You can access individual characters in a string using bracket notation:
+console.log(name.length)  //Length of String: Use .length to get the length of a string:
+console.log(name.toUpperCase())  //Converts string to uppercase
+console.log(name.toLowerCase())  //Converts string in lowercase
+console.log(name.slice(-2,3))        //Extracts a part of a string.
+console.log(name.replace('k',2))     //Replaces part of the string.
+console.log(name.indexOf('e'))  
+
+*/
+
