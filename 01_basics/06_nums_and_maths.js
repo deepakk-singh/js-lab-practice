9ed830562c686e04ba616dfda54dbe7abf531865
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-27 17:45:35 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,10 @@
+/*const score=400
+console.log(score)
+const balance=new Number(100)
+console.log(balance)
+
+console.log(balance.toString())*/
+const otherNumber=1245.895
+console.log(otherNumber.toPrecision(4))
+
+//+++++++++++++++++++++++++++Maths++++++++++++++++++++++++
