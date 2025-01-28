3dd15d8c7a995db626ce29ad50ae8afdf113ce92
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-28 15:07:30 +0000

    Array

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,17 @@
+//arrays
+
+const myArr=[0,1,2,3,4,5]
+  //  console.log(myArr)
+
+ //Arrays method
+ myArr.push(6)
+ //myArr.unshift(9)// to add index[0]
+ myArr.shift()  //to remove at index [0]
+ //console.log(myArr)
+
+//slice or splice
+console.log("A",myArr);
+const myn1 =myArr.slice(1,3)
+
+console.log(myn1)
+console.log("B", myArr)
