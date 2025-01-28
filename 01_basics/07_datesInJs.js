90f078a1c5331965e8b5dae2a873c39ae6a4a60f
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-28 14:46:49 +0000

    date

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -4,5 +4,5 @@ console.log(myDate.toLocaleString())
 console.log(myDate.toDateString())
 console.log(typeof myDate)
 
-let myCreatedDate = new Date(2025,03,04)
+let myCreatedDate = new Date(2025 03 04)
 console.log(myCreatedDate.toDateString())
