7471e6f1891e75e9dbfc27de95481b71585f9d2f
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-28 14:30:22 +0000

    maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -3,8 +3,27 @@ console.log(score)
 const balance=new Number(100)
 console.log(balance)
 
-console.log(balance.toString())*/
+console.log(balance.toString())
 const otherNumber=1245.895
 console.log(otherNumber.toPrecision(4))
-
+ */
 //+++++++++++++++++++++++++++Maths++++++++++++++++++++++++
+
+/*
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(235.4))
+console.log(Math.ceil(2.3))  //it takes only upper value
+console.log(Math.floor(2.9))  //it takes only lower value
+
+*/
+
+//console.log(Math.min(4,1,9,8))  //to find the minimum value from the array
+//console.log(Math.max(4,1,9,8))  //to find the maximum value from the array  
+
+console.log(Math.random())
+console.log(Math.random()*10)
+
+const min=20
+const max=30
+Math.random()
