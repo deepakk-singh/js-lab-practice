e7b50570934e62fa63f7b4cc1204b2df8e903e0f
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-01-28 17:31:42 +0000

    Object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,11 @@
+const jsUser = {
+    name:"Deepak",
+    age:20,
+    emailId:"akjdncd@gmail.com",
+    location:"Jaipur",
+    isLoggedIn:false,
+    lastLoggedIn:["Mondays", "Saturdays"]
+}
+
+console.log(jsUser.emailId)
+console.log(jsUser["emailId"])  //second method to accessing the elements.
