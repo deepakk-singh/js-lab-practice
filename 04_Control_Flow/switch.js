4799973a3881227cf67a639fc5972e6400384d3f
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-02-11 04:17:15 +0000

    switch

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,11 @@
+let day = "Monday";
+switch(day) {
+    case "Monday":
+        console.log("Start of the week");
+        break;
+        case "Friday":
+            console.log("End of the week");
+            break;
+            default:
+                console.log("It's a regular day");
+        }
