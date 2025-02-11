b5955ae1fae6e0de70893692eff3f08056151792
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-02-11 04:42:33 +0000

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// while Loop
+let count = 1;
+while (count <= 3) {
+    console.log("Count:", count);
+    count++;
+}
+
+
+// do-while Loop
+
+let num = 5;
+do {
+    console.log("Number:", num);
+    num--;
+} while (num > 3);
