6a1da5e79c7d4c85f52864902dbf125aa7e44461
Deepak Kumar <117709031+deepakk-singh@users.noreply.github.com>
2025-02-11 04:39:37 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,14 @@
+// for Loop
+
+for (let i = 1; i <= 5; i++) {
+    //console.log("Iteration:", i);
+}
+
+for (let i=0;i<+10;i++)
+{
+    const element = i;
+    if(element==5) {
+        console.log("best number")
+    }
+    console.log(element);
+}
